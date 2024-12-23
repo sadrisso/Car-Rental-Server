74592e5a33d08b5dfbc543351a524e555eb50060
sadrisso <sadriisso304@gmail.com>
2024-12-23 23:02:08 +0600

    add myBookings api

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const carsCollection = client.db("carsDB").collection("cars");
         const bookingCollection = client.db("carsDB").collection("bookings")
@@ -56,6 +56,14 @@ async function run() {
             const email = req.params.email;
             const query = { userEmail: email }
             const result = await carsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+        app.get("/my-bookings/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email }
+            const result = await bookingCollection.find(query).toArray()
             res.send(result)
         })
 
@@ -131,8 +139,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
